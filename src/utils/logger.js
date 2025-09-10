a588c78c13f2ebd36752ed51b9452739064a9ae0
Gabriel Vellasco de Mattos <53792961+vellascogabriel@users.noreply.github.com>
2025-09-10 00:10:29 -0300

    feature(logger): create logger configuration

---
+++ src/utils/logger.js
@@ -0,0 +1,17 @@
+const pino = require('pino');
+const config = require('../../config/default');
+
+
+// Logger configuration based on project configuration
+const loggerOptions = {
+    level: config.logger.level,
+    transport: config.logger.prettyPrint ? 
+    {
+        target: 'pino-pretty',
+        options: {
+            colorize: true,
+            translate: 'SYS:standard',
+            ignore: 'pid,hostname'
+        }
+    } : undefined
+}
